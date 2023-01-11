77744a6e1b3b4474ee52b27c1c38207042fac4c6
Edison Osorio Pérez <eosorio@celerix.com>
2023-01-11 10:35:45 -0500

    comment module shared

--- src/app/shared/shared.module.ts
+++ src/app/shared/shared.module.ts
@@ -1,6 +1,7 @@
 import { AngularMaterialModule } from './../angular-material/angular-material.module';
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
+// Module in translate
 import { TranslateModule } from '@ngx-translate/core';
 
 @NgModule({
